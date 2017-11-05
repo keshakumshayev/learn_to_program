3fbd3046b9080d9c74531a532d5946d2b960f803
k-e-w-a <kumshkesh@gmail.com>
2017-11-05 22:06:37 +0000

    chapter 5 exercises

---
+++ chapter5/fullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts "Hello, what's your first name?"
+fname = gets.chomp
+puts "What about your middle name?"
+mname = gets.chomp
+puts "And what is your last name?"
+lname = gets.chomp
+puts "Good to meet you, #{fname+' '+mname+' '+lname}."
---
+++ chapter5/yourFavoriteNumber.rb
@@ -0,0 +1,3 @@
+puts "What's your favorite number?"
+favNum = gets.chomp.to_i
+puts "#{favNum} is alright, but #{favNum+1} is bigger and better. #{favNum+1} could be your new favorite number!"
