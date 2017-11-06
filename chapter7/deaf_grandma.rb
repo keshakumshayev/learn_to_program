7afb2f163d8724bcd5d6ac32955b520ef8259109
k-e-w-a <kumshkesh@gmail.com>
2017-11-06 17:27:22 +0000

    updated ruby to 2.0, edited deaf grandma exercise to use rand instead of choice

--- chapter7/deaf_grandma.rb
+++ chapter7/deaf_grandma.rb
@@ -1,10 +1,8 @@
 puts "Say something to grandma, LOUDLY SO SHE CAN HEAR!"
 
-#written in ruby v1.8.7 using choice instead of rand
-
 while user_input = gets.chomp and user_input != user_input.upcase
 	puts "HUH?! SPEAK UP, SONNY!"
 end
 if user_input == user_input.upcase
-	puts "NO, NOT SINCE #{(1930..1950).map{|e|e}.choice}!"
+	puts "NO, NOT SINCE #{rand(1930..1950)}!"
 end
