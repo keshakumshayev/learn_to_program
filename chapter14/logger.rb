e53de9116fe2a05f00d683a26255cc230a2de6ef
k-e-w-a <kumshkesh@gmail.com>
2017-11-09 01:09:29 +0000

    log exercises chapter 14

--- chapter14/logger.rb
+++ chapter14/logger.rb
@@ -1,21 +1,13 @@
-$depth = 0
-
 def logger(block_description, &block)
-	$depth += 1
-	puts indent+"Beginning '#{block_description}'"
+	puts "Beginning '#{block_description}'"
 	puts
 	block.call
-	puts indent+"Ending '#{block_description}'"
-	$depth -= 1
-end
-
-def indent
-	'   '*($depth-1)
+	puts "Ending '#{block_description}'"
 end
 
 logger('25000 doublings') do
 	number = 2**25000
-	puts indent+"#{number.to_s.length} digits"
+	puts "#{number.to_s.length} digits"
 	puts
 	# That's the number of digits in this HUGE number.
 	logger('count to a million') do
@@ -23,19 +15,19 @@ logger('25000 doublings') do
 		1000000.times do
 		number = number + 1
 		end
-		puts indent+number.to_s
+		puts number.to_s
 		puts
 	end
 
 	logger('25000 doublings') do
 		number = 2**25000
-		puts indent+"#{number.to_s.length} digits"
+		puts "#{number.to_s.length} digits"
 		puts
 		logger('another block') do
 			puts
 			puts
 			puts
-			puts indent+'this is the last block'
+			puts 'this is the last block'
 			puts
 			puts
 			puts
---
+++ chapter14/logger_xl.rb
@@ -0,0 +1,43 @@
+$depth = 0
+
+def logger(block_description, &block)
+	$depth += 1
+	puts indent+"Beginning '#{block_description}'"
+	puts
+	block.call
+	puts indent+"Ending '#{block_description}'"
+	$depth -= 1
+end
+
+def indent
+	'   '*($depth-1)
+end
+
+logger('25000 doublings') do
+	number = 2**25000
+	puts indent+"#{number.to_s.length} digits"
+	puts
+	logger('count to a million') do
+		number = 0
+		1000000.times do
+		number = number + 1
+		end
+		puts indent+number.to_s
+		puts
+	end
+
+	logger('25000 doublings') do
+		number = 2**25000
+		puts indent+"#{number.to_s.length} digits"
+		puts
+		logger('another block') do
+			puts
+			puts
+			puts
+			puts indent+'this is the last block'
+			puts
+			puts
+			puts
+		end
+	end
+end
